9507c8ab22f1b12fe2a556c9743b207def406aa1
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-01-27 13:16:18 -0500

    Fixed text-leak bug for when expanded text is hidden.

--- lc_plmod/me/ignore_tag.pm
+++ lc_plmod/me/ignore_tag.pm
@@ -11,9 +11,13 @@ $intag->default_hnd(\&nulldo,\&nulldo);
 
 sub tag_on {
   $intag->claim($_[1]);
+  
+  $_[1]->{'tagdata'}->{'ochar'} = $_[0]->{'fnc'}->{'char'};
+  $_[0]->{'fnc'}->{'char'} = \&nulldo;
 }
 
 sub tag_off {
+  $_[0]->{'fnc'}->{'char'} = $_[1]->{'tagdata'}->{'ochar'};
 }
 
 
